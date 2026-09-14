1839cca2085eec65a6e73e52e0a3ef1c6295f3d7
Ervin <ervinsmeed@gmail.com>
2026-09-14 14:02:52 +0600

    organize translation resources

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,4 @@
-import { ru, en } from "./lib/additionalTranslations";
+import { ru, en } from "./locales/translations";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
